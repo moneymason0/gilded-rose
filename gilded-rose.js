9b5153af3f607049589223e47bf73b2d06f8c652
Mason Money <moneymason0@gmail.com>
2023-03-16 14:18:28 -0600

    First and final commit, got all the requirements done

--- gilded-rose.js
+++ gilded-rose.js
@@ -1,67 +1,72 @@
-export class Item {
+class Item {
   constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
+  updateQuality(){
+    this.sellIn--;
+  }
 }
+
+export class Depreciate extends Item{
+  constructor(name, sellin, quality){
+    super(name, sellin, quality);
+    this.valueType = "Depreciate";
+  }
+  updateQuality(){
+    super.updateQuality();
+    if (this.quality > 0)
+        this.quality--;
+    if ((this.sellIn < 0 || this.name.includes("Conjured")) && this.quality > 0)
+        this.quality--;
+  }
+}
+
+export class Appreciate extends Item{
+  constructor(name, sellin, quality){
+    super(name, sellin, quality);
+    this.valueType = "Appreciate";
+}
+  updateQuality(){
+    super.updateQuality();
+    if (this.quality < 50) {
+      this.quality = this.quality + 1;
+      if (this.name == "Backstage passes to a TAFKAL80ETC concert") {
+        if (this.sellIn < 11 && this.quality < 50) 
+            this.quality++;
+        if (this.sellIn < 6 && this.quality < 50) 
+            this.quality++;
+        if (this.sellIn < 0) 
+            this.quality = 0;
+        }
+      }  
+  }
+}
+  
+export class Stagnate extends Item{
+  constructor(name, sellin, quality){
+    super(name, sellin, quality);
+    this.valueType = "Stagnate";
+}
+
+  updateQuality(){
+    // Do nothing
+  }
+}
+
 
 export let items = [];
 
-items.push(new Item("+5 Dexterity Vest", 10, 20));
-items.push(new Item("Aged Brie", 2, 0));
-items.push(new Item("Elixir of the Mongoose", 5, 7));
-items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-items.push(new Item("Conjured Mana Cake", 3, 6));
+items.push(new Depreciate("+5 Dexterity Vest", 10, 20));
+items.push(new Appreciate("Aged Brie", 2, 0));
+items.push(new Depreciate("Elixir of the Mongoose", 5, 7));
+items.push(new Stagnate("Sulfuras, Hand of Ragnaros", 0, 80));
+items.push(new Appreciate("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+items.push(new Depreciate("Conjured Mana Cake", 3, 6));
 
-export const updateQuality = () => {
+export const updateQualityAll = () => {
   for (let item of items) {
-    if (
-      item.name != "Aged Brie" &&
-      item.name != "Backstage passes to a TAFKAL80ETC concert"
-    ) {
-      if (item.quality > 0) {
-        if (item.name != "Sulfuras, Hand of Ragnaros") {
-          item.quality = item.quality - 1;
-        }
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
-      }
-    }
-    if (item.name != "Sulfuras, Hand of Ragnaros") {
-      item.sellIn = item.sellIn - 1;
-    }
-    if (item.sellIn < 0) {
-      if (item.name != "Aged Brie") {
-        if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-          if (item.quality > 0) {
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-              item.quality = item.quality - 1;
-            }
-          }
-        } else {
-          item.quality = item.quality - item.quality;
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
-    }
-  }
-};
+        item.updateQuality();
+  };
+}
--- gilded-rose.spec.js
+++ gilded-rose.spec.js
@@ -1,14 +1,97 @@
-import { expect, describe, it } from "vitest";
-import { Item, items, updateQuality } from "./gilded-rose.js";
+import { expect, describe, test } from "vitest";
+import { Appreciate, Depreciate, Stagnate, items, updateQualityAll } from "./gilded-rose.js";
 
 describe("updateQuality", () => {
-  it("reduces quality and sellIn of basic items by 1", () => {
-    const testItem = new Item("basic", 5, 3);
+  test("reduces quality and sellIn of basic items by 1", () => {
+    const testItem = new Depreciate("basic", 5, 3); // Type, Sellin, Quality
+    console.log("This is in the test file", testItem.valueType);
     items.push(testItem);
 
-    updateQuality();
+    updateQualityAll();
 
     expect(testItem.quality).toBe(2);
     expect(testItem.sellIn).toBe(4);
   });
+
+  test("reduces sellIn less than zero", () => {
+    const testItem = new Depreciate("basic", -1, 5); // Type, Sellin, Quality
+    items.push(testItem);
+
+    updateQualityAll();
+
+    expect(testItem.quality).toBe(3);
+    expect(testItem.sellIn).toBe(-2);
+  });
+
+  test("Aged Brie increase in quality", () => {
+    const testItem = new Appreciate("Aged Brie", 4, 6); // Type, Sellin, Quality
+    items.push(testItem);
+
+    updateQualityAll();
+
+    expect(testItem.quality).toBe(7);
+    expect(testItem.sellIn).toBe(3);
+  });
+
+  test("no items (Aged Brie) go past quality of 50", () => {
+    const testItem = new Appreciate("Aged Brie", 3, 50); // Type, Sellin, Quality
+    items.push(testItem);
+
+    updateQualityAll();
+
+    expect(testItem.quality).toBe(50);
+    expect(testItem.sellIn).toBe(2);
+  });  
+
+  test("Sulfuras, Hand of Ragnaros never decreases in quality", () => {
+    const testItem = new Stagnate("Sulfuras, Hand of Ragnaros", 4, 80); // Type, Sellin, Quality
+    items.push(testItem);
+
+    updateQualityAll();
+
+    expect(testItem.quality).toBe(80);
+    expect(testItem.sellIn).toBe(4); // Sellin date for legendary doesnt go down
+  });
+
+  test("Backstage passes to a TAFKAL80ETC concert, 9 days left before concert", () => {
+    const testItem = new Appreciate("Backstage passes to a TAFKAL80ETC concert", 9, 20); // Type, Sellin, Quality
+    items.push(testItem);
+
+    updateQualityAll();
+
+    expect(testItem.quality).toBe(22);
+    expect(testItem.sellIn).toBe(8); 
+  });
+
+  test("Backstage passes to a TAFKAL80ETC concert, 4 days left before concert", () => {
+    const testItem = new Appreciate("Backstage passes to a TAFKAL80ETC concert", 4, 20); // Type, Sellin, Quality
+    items.push(testItem);
+
+    updateQualityAll();
+
+    expect(testItem.quality).toBe(23);
+    expect(testItem.sellIn).toBe(3); 
+  });
+
+  test("Backstage passes to a TAFKAL80ETC concert, after concert", () => {
+    const testItem = new Appreciate("Backstage passes to a TAFKAL80ETC concert", -1, 20); // Type, Sellin, Quality
+    items.push(testItem);
+
+    updateQualityAll();
+
+    expect(testItem.quality).toBe(0);
+    expect(testItem.sellIn).toBe(-2); 
+  });
+
+  test("Conjured items degrade twice as fast", () => {
+    const testItem = new Depreciate("Conjured Mana Cake", 3, 6); // Type, Sellin, Quality
+    items.push(testItem);
+
+    updateQualityAll();
+
+    expect(testItem.quality).toBe(4);
+    expect(testItem.sellIn).toBe(2); 
+  });
 });
+
+// Not sure how to test "The quality of an item is never negative."
